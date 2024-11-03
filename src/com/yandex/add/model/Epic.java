570688b8bd69c6efbe35519cd3e2d67913b8576c
LitaterLa <litater@rambler.ru>
2024-11-03 10:05:19 +0500

    FileBackedTaskManager added (sprint 7)

--- src/com/yandex/add/model/Epic.java
+++ src/com/yandex/add/model/Epic.java
@@ -5,6 +5,10 @@ public class Epic extends Task {
         super(title, description);
     }
 
+    public Epic(String title, String epic1, int i) {
+        super(title, epic1, i);
+    }
+
     @Override
     public TaskType getType() {
         return TaskType.EPIC;
--- test/com/yandex/add/service/history/InMemoryHistoryManagerTest.java
+++ test/com/yandex/add/service/history/InMemoryHistoryManagerTest.java
@@ -3,7 +3,6 @@ package com.yandex.add.service.history;
 import com.yandex.add.model.Epic;
 import com.yandex.add.model.Subtask;
 import com.yandex.add.model.Task;
-import com.yandex.add.service.taskManager.InMemoryTaskManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,19 +14,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
     InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-    InMemoryTaskManager taskManager = new InMemoryTaskManager(historyManager);
     private Task task1, task2, task3;
     private List<Task> history;
 
     @BeforeEach
     public void setup() throws IOException {
         history = new ArrayList<>();
-        task1 = new Task("title1", "description");
-        task2 = new Task("title2", "description");
-        task3 = new Task("title3", "description");
-        taskManager.createTask(task1);
-        taskManager.createTask(task2);
-        taskManager.createTask(task3);
+        task1 = new Task("title1", "description", 300);
+        task2 = new Task("title2", "description", 301);
+        task3 = new Task("title3", "description", 302);
         historyManager.add(task1);
         historyManager.add(task2);
         historyManager.add(task3);
@@ -57,25 +52,23 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void shouldNotSaveOldID() throws IOException {
-        taskManager.deleteTaskById(task1.getIdNum());
+    void shouldNotSaveOldID() {
+        historyManager.remove(task1.getIdNum());
         Task newTask = new Task("new", "newer");
         assertNotEquals(task1.getIdNum(), newTask.getIdNum());
     }
 
     @Test
-    void shouldNotHoldDisusedSubtaskId() throws IOException {
-        Epic epic = new Epic("epic", "epic");
-        taskManager.createEpic(epic);
-        Subtask subtask1 = new Subtask("subtask", "subtask", epic.getIdNum());
-        Subtask subtask2 = new Subtask("subtask", "subtask", epic.getIdNum());
-        taskManager.createSubtask(subtask1);
-        taskManager.createSubtask(subtask2);
-        taskManager.deleteSubtaskById(subtask1.getIdNum());
+    void shouldNotHoldDisusedSubtaskId() {
+        Epic epic = new Epic("epic", "epic", 5);
+        Subtask subtask1 = new Subtask("subtask", "subtask", 5);
+        Subtask subtask2 = new Subtask("subtask", "subtask", 5);
+        historyManager.add(epic);
+        historyManager.add(subtask1);
+        historyManager.add(subtask2);
         historyManager.remove(subtask1.getIdNum());
-        Subtask subtask = taskManager.getSubtaskByID(subtask1.getIdNum());
-        List<Subtask> subtasks = taskManager.getSubtasks();
-        assertEquals(1, subtasks.size());
+        List<Task> subtasks = historyManager.getHistory();
+        assertEquals(4, subtasks.size());
     }
 
     @Test
@@ -84,26 +77,7 @@ class InMemoryHistoryManagerTest {
         history1.add(task1);
         history1.add(task2);
         history1.add(task3);
-        taskManager.getTaskById(task1.getIdNum());
-        taskManager.getTaskById(task2.getIdNum());
-        taskManager.getTaskById(task3.getIdNum());
         List<Task> history2 = historyManager.getHistory();
         assertEquals(history1.size(), history2.size());
     }
-
-    @Test
-    void shouldFunctionManagerIfPropertiesChange() throws IOException {
-        task1.setTitle("new title");
-        task1.setDescription("new description");
-        taskManager.updateTask(task1);
-        List<Task> tasks = historyManager.getHistory();
-        Task taskProb = new Task(null, null);
-        for (Task task : tasks) {
-            if (task.equals(task1)) {
-                taskProb = task;
-            }
-        }
-        assertEquals(task1, taskProb);
-    }
-
 }
